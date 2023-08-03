1c4d66902074cf604243aca97bc37696199c6d8a
JeremyChim <jer888chim@outlook.com>
2023-08-03 13:19:51 +0800

    Update read.py

--- read.py
+++ read.py
@@ -8,9 +8,14 @@
 import openpyxl as op
 
 def ret_val(x:int, y:int):
+    '''
+    :param x: 单元格行数
+    :param y: 单元格列数
+    :return: 单元格的值
+    '''
     wb = op.load_workbook('report.xlsx')
     ws = wb.worksheets[0]
-    a = ws.cell(x,y).value
+    a = ws.cell(x, y).value
     return a
 
 if __name__ == '__main__':
@@ -18,5 +23,5 @@ if __name__ == '__main__':
     cmd = 'copy case.xlsx report.xlsx'
     system(cmd)
 
-    a = ret_val(1,2)
+    a = ret_val(1, 2)
     print(a.__class__, a)
